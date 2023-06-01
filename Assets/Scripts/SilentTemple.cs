3466ad525a91bac6a5b29f8e384a1ba367c40666
Joamoi <joni.aitto.oja@hotmail.com>
2023-06-01 10:23:45 +0300

    Silent temple

---
+++ Assets/Scripts/SilentTemple.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SilentTemple : MonoBehaviour
+{
+    public AudioSource daySound;
+    public AudioSource nightSound;
+
+    private float daySoundVolume;
+    private float nightSoundVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        daySoundVolume = daySound.volume;
+        nightSoundVolume = nightSound.volume;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            daySound.volume = 0.3f * daySoundVolume;
+            nightSound.volume = 0.3f * nightSoundVolume;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            daySound.volume = daySoundVolume;
+            nightSound.volume = nightSoundVolume;
+        }
+    }
+}
